725852a7d86d52fff26dd90234d1971d1d507228
Mujahid <mujahid.170997@gmail.com>
2021-05-16 14:44:12 +0700

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
+import React from 'react';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -22,7 +22,7 @@ function App() {
       .catch((err) => {
         console.log(err);
       });
-    setWord("");
+    setWord('');
   };
 
   return (
